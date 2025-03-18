6c66ef275ed3e97a7f080733372f17276e442cfb
ravikumar <raviluhaniwal318@gmail.com>
2025-03-18 09:48:34 +0530

    function component

---
+++ React JS/myapp/src/App.js
@@ -0,0 +1,36 @@
+// Create a function component 
+
+import React from "react";
+// import a function component
+import Home from "./components/Home.jsx"
+
+function App(){
+  return(
+
+    // JSX fragments
+    <>
+
+    
+    {/* call a function component */}
+    <Home/>
+    <Home/>
+    <Home/>
+    
+    <h1>Helooo</h1>
+    <Home/>
+    <Home/>
+    <Home/>
+    <Home/>
+    <Home/>
+    <Home/>
+    <Home/>
+
+    </>
+
+
+  )
+}
+
+
+export default App
+
---
+++ React JS/myapp/src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur,
+        maxime soluta. Facilis, et nobis? Labore beatae laboriosam aliquam quae,
+        accusamus magni nostrum ullam ipsum, dignissimos, tenetur architecto
+        delectus! Vel, doloremque!
+      </p>
+    </>
+  );
+}
+
+
+export default Home
