a25cc9632160c164bf48ab66a7e2f92d8f2dc1cf
ravikumar <raviluhaniwal318@gmail.com>
2025-03-20 11:13:44 +0530

    javascript ram

---
+++ Javascript/Sahil/VAribales.js
@@ -0,0 +1,43 @@
+
+// Keywords 
+// let var const 
+//  Javascript comment // 
+// Deaclare a variable
+
+let x = 10
+let _ram = 10
+let $sahil = 20
+let X = 20
+
+console.log(_ram)
+
+// let = keyword 
+// x = varibale name 
+// = ==> assignment operator 
+// 10 = data 
+console.log(x)
+//  re-assigmnet 
+ x = 20
+
+ console.log(x)
+
+
+
+// let x  = 20
+
+//  block scope
+{
+    let y = 10
+    console.log("y = ", y)
+}
+
+var a = 19
+var a = 10
+
+{
+    var b = 10
+}
+
+console.log("b",b)
+
+
