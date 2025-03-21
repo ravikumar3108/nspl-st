b6d7f49edecd6b79a250f673df4cc249b2ce661f
ravikumar <raviluhaniwal318@gmail.com>
2025-03-21 09:36:12 +0530

    css

--- React JS/myapp/src/App.js
+++ React JS/myapp/src/App.js
@@ -1,29 +1,19 @@
 // Create a function component 
-
 import React from "react";
 // import a function component
 import Home from "./components/Home.jsx"
+import "./App.css"
 
 function App(){
   return(
 
     // JSX fragments
     <>
-
-    
     {/* call a function component */}
     <Home/>
-    <Home/>
-    <Home/>
-    
+     
     <h1>Helooo</h1>
-    <Home/>
-    <Home/>
-    <Home/>
-    <Home/>
-    <Home/>
-    <Home/>
-    <Home/>
+    
 
     </>
 
--- React JS/myapp/src/components/Home.jsx
+++ React JS/myapp/src/components/Home.jsx
@@ -1,17 +1,29 @@
 import React from "react";
+import image from "./Images/dark-style-valentines-day-celebration.jpg"
 
 function Home() {
+  let name = "Dherraj Kumar";
+
+  let age = 43;
   return (
     <>
-      <p>
+      <h1>
+        Heloo My name is {name} ,{age}
+      </h1>
+
+      <p
+        id="paara"
+        className="para"
+        style={{ color: "white", backgroundColor: "black", padding: "20px" }}
+      >
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur,
         maxime soluta. Facilis, et nobis? Labore beatae laboriosam aliquam quae,
         accusamus magni nostrum ullam ipsum, dignissimos, tenetur architecto
         delectus! Vel, doloremque!
       </p>
+      <img src={image} alt="" width="100%"/>
     </>
   );
 }
 
-
-export default Home
+export default Home;
