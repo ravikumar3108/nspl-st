dd2779910de9cf46d0ab6ae1c6025d6c86d77495
ravikumar <raviluhaniwal318@gmail.com>
2025-05-10 12:11:51 +0530

    js

---
+++ Harish/Javascript/index.js
@@ -0,0 +1,66 @@
+
+// Output 
+
+console.log("Heloo HArish")
+// document.write("Heloo HArish")
+console.warn("Warning")
+console.error("Error")
+
+// alert("This Page Beloongs to JAvascript")
+
+
+// Variables :- Variables are used to store our data. 
+
+// create a variables (declare a varible)
+x = 10  // integer 
+
+console.log(x)
+
+//  names of our variable 
+// alphabets 
+// alphabets + number 
+x10 = 30
+//  underscore 
+_harish = "harish"
+// $dollar
+$kumar = 10
+// camelcase
+harsihKumar = 20
+//  pascal case 
+HarishKumar = 30
+
+// keywords
+// let ,var, const
+
+let y = 10
+// let y = 39
+console.log("y is = ", y)
+// re-assignment 
+y = 30
+
+console.log("y is = ", y)
+// 1. it cannot be re-decalred 
+// 2. it is a block scope 
+
+
+{
+    let xy = 10
+}
+// console.log(xy)
+
+
+// 2. var 
+// 1. it can be re-decalred 
+// 2. it cannot be  a block scope 
+
+
+// 3. const 
+// 1. it cannot be re-decalred 
+// 2. it is a block scope 
+// 3. it cannot be re-assign 
+
+
+const a = 10
+a = 20
+console.log(a)
+
