740e000fc53e53bb33686a246f537c3dc4a84370
ravikumar <raviluhaniwal318@gmail.com>
2025-05-15 15:48:37 +0530

    javascript operators

--- Harish/Javascript/index.js
+++ Harish/Javascript/index.js
@@ -61,6 +61,100 @@ console.log("y is = ", y)
 
 
 const a = 10
-a = 20
-console.log(a)
+// a = 20
+// console.log(a)
+
+
+// Operators :- 
+// 1. Arithmetic operators 
+// + 
+// -
+// *
+// /
+// % modulus == remainder 
+// ** expontation
+// ++ increment 
+// -- decrement 
+
+// let a1 = 10
+// let b = 20
+// let c = a1 + b
+// let d = c * 2
+
+// console.log(c)
+// console.log("d is ", d)
+
+// let c1 = 10
+// let c2 = 3
+// console.log(c1 % c2)
+// console.log(c1 ** c2)
+
+// let x12 = 30
+// // prefix 
+// let inc = ++x12
+// console.log(x12)
+// console.log(inc)
+
+// // postfix
+
+// let inc1 = x12++ 
+// console.log(x12)
+// console.log(inc1)
+
+// 2. Assignment operators
+// = assign operator
+// += 
+// -=
+// *=
+//  /=
+// %=
+// **=
+
+let a1 = 10
+let b = 20
+
+let c = a1 + 10
+console.log(c)
+
+
+a1 += 10
+
+
+console.log(a1)
+
+
+
+
+
+
+
+
+// 3. Comparision operators (boolean)
+// == equal value 
+// === equal value and equal type 
+// !== vice versa
+// != vice versa
+// >
+// <
+// >=
+// <=
+
+let ab = 10
+let aa = 20
+let ac = "20"
+
+console.log(aa == ac)
+console.log(aa === ac)
+console.log(aa !== ac)
+console.log(ab == ac)
+console.log(ab != ac)
+
+
+
+
+// 4. logical operators
+// and &&
+//  or ||
+//  not ! 
+
 
