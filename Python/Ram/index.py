0feb18ad3834efd4c9feed3bf721a4048db06f6d
ravikumar <raviluhaniwal318@gmail.com>
2025-05-15 16:53:10 +0530

    python

---
+++ Python/Ram/index.py
@@ -0,0 +1,68 @@
+# print("Helooo Ram")
+
+# # comments 
+# """ 
+#     Heloo this is comment 
+# """
+
+# x = """Heloo This is String"""
+# print(x)
+
+# x = 130
+# y = 20
+
+# if x > y :
+#    print("5 si greater")
+# else:
+#    print("Else Stateement")
+# print("heloo")
+
+
+# x = str(3)
+# y = int(4)
+# z = float(4)
+
+# print(x)
+# print(type(x))
+
+
+#  Conditions
+
+x = 10
+y = 20
+
+if x == y :
+    print("x is equal to y")
+elif x < y:
+    print("y is greater than x")
+elif x < y:
+    print("y is greater than x")
+elif x < y:
+    print("y is greater than x")
+elif x < y:
+    print("y is greater than x")
+elif x < y:
+    print("y is greater than x")
+else:
+    print("Something Error")
+
+
+
+a = 1
+while a <= 20:
+    print(a)
+    if a == 10:
+        break
+    a += 1
+
+b = 0
+while b <= 20:
+    b += 1
+    if b == 10:
+        continue
+    print(b)
+
+
+
+for i in range(6):
+    print(i)
---
+++ Python/Subh/index.py
@@ -0,0 +1,62 @@
+
+#  Varialbes :- 
+x = 10
+print(x)
+
+# Variables :- 
+# alphabets 
+x = 10
+# names
+subh = 20
+
+# _underscore
+_y = 30
+_subh = 20
+# variables name is not start with a Number.
+# 10 = 30
+#  aphabets + number 
+x10 = 30
+
+# camelCase
+myVariable = 10
+
+#  PascalCase
+MyVariable = 30
+
+# Snake Case
+my_variabels = 20
+
+#  Case Sentitive
+xy = 10
+XY = 20
+print("small xy is" ,xy)
+print("Big XY is",XY)
+
+# Decalare / Create a variables 
+a = 10
+print("a is",a)
+
+print(a + 30)
+
+#  re-assignment 
+a = 30
+print("after re-assignment a is",a)
+
+# multiple variable
+x , y , z = 12 , 2 , 45
+print(z)
+
+x = y = z = "Shubh"
+print(x)
+print(y)
+print(z)
+
+# Type of 
+print(type(x))
+
+
+
+
+
+
+
