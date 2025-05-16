c8ed7a55bd7fc89cb6b7c4c511539524cedadf9d
ravikumar <raviluhaniwal318@gmail.com>
2025-05-16 17:55:41 +0530

    data types

---
+++ Python/Subh/DataTypes.py
@@ -0,0 +1,58 @@
+
+# Data Types :-  
+
+#  Integer 
+x = 10
+
+y = 12.90
+print(type(y))
+
+# Complex Number
+y = 20j
+print(type(y))
+
+# String  : it is used to store character values.
+y = "HEllo Shubh"
+print("first variable type",type(y))
+
+y = 'heloo'
+print(y)
+print(type(y))
+
+y = """Heloo My name is Shubh."""
+print(y)
+
+# single line comments 
+""" Multiple line Comments """
+
+# Bollean  = (true , false) 
+x = True
+y = False
+print(type(x))
+
+# list :- it is used to store multile data / items
+
+xy = ["A","B","C","D",1,23,]
+print(xy)
+print(type(xy))
+
+
+# Set : it is used to store multile data / items
+xy = {"A","B","C","D",1,23,}
+print(type(xy))
+
+# tuple : it is used to store multile data / items
+xy = ("A","B","C","D",1,23,)
+print(type(xy))
+
+
+# Dictionary :- it is used to store multile data / items in the form of key and values.
+
+y = {
+    "name" : "Shubh",
+    "Age" : 12,
+}
+print(y)
+print(type(y))
+
+
--- Python/Subh/index.py
+++ Python/Subh/index.py
@@ -1,58 +1,58 @@
 
 #  Varialbes :- 
-x = 10
-print(x)
+# x = 10
+# print(x)
 
-# Variables :- 
-# alphabets 
-x = 10
-# names
-subh = 20
+# # Variables :- 
+# # alphabets 
+# x = 10
+# # names
+# subh = 20
 
-# _underscore
-_y = 30
-_subh = 20
-# variables name is not start with a Number.
-# 10 = 30
-#  aphabets + number 
-x10 = 30
+# # _underscore
+# _y = 30
+# _subh = 20
+# # variables name is not start with a Number.
+# # 10 = 30
+# #  aphabets + number 
+# x10 = 30
 
-# camelCase
-myVariable = 10
+# # camelCase
+# myVariable = 10
 
-#  PascalCase
-MyVariable = 30
+# #  PascalCase
+# MyVariable = 30
 
-# Snake Case
-my_variabels = 20
+# # Snake Case
+# my_variabels = 20
 
-#  Case Sentitive
-xy = 10
-XY = 20
-print("small xy is" ,xy)
-print("Big XY is",XY)
+# #  Case Sentitive
+# xy = 10
+# XY = 20
+# print("small xy is" ,xy)
+# print("Big XY is",XY)
 
-# Decalare / Create a variables 
-a = 10
-print("a is",a)
+# # Decalare / Create a variables 
+# a = 10
+# print("a is",a)
 
-print(a + 30)
+# print(a + 30)
 
-#  re-assignment 
-a = 30
-print("after re-assignment a is",a)
+# #  re-assignment 
+# a = 30
+# print("after re-assignment a is",a)
 
-# multiple variable
-x , y , z = 12 , 2 , 45
-print(z)
+# # multiple variable
+# x , y , z = 12 , 2 , 45
+# print(z)
 
-x = y = z = "Shubh"
-print(x)
-print(y)
-print(z)
+# x = y = z = "Shubh"
+# print(x)
+# print(y)
+# print(z)
 
-# Type of 
-print(type(x))
+# # Type of 
+# print(type(x))
 
 
 
