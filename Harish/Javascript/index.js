3c81c40c0579e00706bc6ff0d719398d69c1f817
ravikumar <raviluhaniwal318@gmail.com>
2025-05-27 14:46:17 +0530

    fun

--- Harish/Javascript/index.js
+++ Harish/Javascript/index.js
@@ -210,21 +210,21 @@ console.log(or)
 // parseInt()
 
 
-let a = parseInt(prompt("Enter a Number"))
-let b = 11 
+// let a = parseInt(prompt("Enter a Number"))
+// let b = 11 
 
-if (a === b) {
-    console.log("a is equal to b")
-}
-else if (a > b) {
-    console.log("a is greater than b")
-}
-else if (a < b) {
-    console.log("a is less than b")
-}
-else {
-    console.log("a is not equal to b ")
-}
+// if (a === b) {
+//     console.log("a is equal to b")
+// }
+// else if (a > b) {
+//     console.log("a is greater than b")
+// }
+// else if (a < b) {
+//     console.log("a is less than b")
+// }
+// else {
+//     console.log("a is not equal to b ")
+// }
 
 //  Calculator 
 // a , b , c 
@@ -237,7 +237,131 @@ else {
 // 60 or 70 k bich me Grade C 
 
 // 9.	Check if a number is divisible by both 3 and 5 
-
-
 // 6.	Find the largest among three numbers
+
+// Array :- 
+// let arr = [1,2,4,4,5,6,6,7,7]
+// // aaray works on index number (0 to .....)
+
+// console.log(arr)
+
+// // array length
+// console.log(arr.length)
+
+// // Access the values of our array 
+// a = arr[8]
+// console.log(a)
+
+// // replcae the data 
+// arr[3] = "Harish"
+// console.log(arr)
+
+// // Methods 
+// // pop() = this method used to remove last value 
+// // push() this method used to add last value 
+
+// arr.pop()
+// console.log(arr)
+
+// arr.push("Hello")
+// console.log(arr)
+
+// // shift()
+// // unshift()
+// arr.shift()
+// console.log(arr)
+
+// // slice : it is used to cut a new array in the existing array.
+// // splice :- add data and remove data in the specific index number 
+
+// a = arr.slice(0,5)
+// // note :- last value is not included 
+// console.log(a)
+
+// // 1. index number 
+// // 2. remove items 
+// // 3. add items 
+// arr.splice(2,3,"h","r","i")
+// console.log(arr)
+
+// // concat method :- combine one or more aaray 
+// let arr1 = ["12",4,5,6,8,90,]
+// let arr2 = ["120",40,50,60,80,90,]
+// let arr3 = ["1200",400,500,600,800,900,]
+
+// conc = arr1.concat(arr2,arr3)
+// console.log(conc)
+
+// delete arr1[3]
+// console.log(arr1)
+// // sort 
+// // reverse
+// arr1.reverse()
+// console.log(arr1)
+
+
+
+// let str = "                                    hii my name is ravi. ravi "
+//  Aceesss our string []
+// length
+//  replace() => 2 parameters = first Exist in your sting  , second = replace by second value 
+//  replaceAll()
+// toUpperCase()
+// toLowerCase()
+// concat() 
+// slice() 
+// trim()
+// trimStart()
+// trimEnd()
+// charAt()
+
+
+// Functions :- it is a block of code which is run when our function is call 
+// Create a function 
+
+function MyFunction() {
+
+    console.log("funnction runn ")
+
+    a = 10
+    b = 30
+    console.log(a + b)
+
+}
+
+// calling a function 
+MyFunction()
+
+
+//  Parameters :- which is declare into the function parenthesis
+// and argumnets :- which is declare into the function calling parenthesis
+// default parameters 
+
+
+function MyFunction1(a, b, c = 30) {
+
+    console.log("funnction runn ")
+
+    console.log(a + b + c)
+
+}
+MyFunction1(10, 20, 45)
+
+
+// return 
+
+function MyFunction1(a, b, c = 30) {
+
+    console.log("funnction runn ")
+
+    x = a + b + 30
+    return x
+
+}
+ab = MyFunction1(a = 10, c = 20, b = 45)
+console.log(ab * 10)
+ab * 10
+
+
+
 
