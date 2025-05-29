e16a367d8c42e5f18d4e8ed8717779c0c169e4a6
ravikumar <raviluhaniwal318@gmail.com>
2025-05-29 15:01:58 +0530

    js

---
+++ Javascript/Vedparksh/index.js
@@ -0,0 +1,92 @@
+
+console.log("External file")
+
+// Javascript is a programing language.It is scripted, interpted, precomplied and create dynamic features to web pages.
+// Complier = convert English language(coding language) to machine(laptop, computer) language. (Binary Number 0,1) 
+
+
+// let x = 10
+// let x = 20
+console.error("Error")
+console.warn("Warning")
+// alert("Alert On page")
+
+
+// Variables :- Variables are used to store our data/values.
+
+// create a variables 
+// x = 10
+// // x is variabel name 
+// // = is a assignment operator
+// // 10 is data 
+// console.log("x is",x)
+
+// variable names :- 
+// alphabets , names ,
+let s = 10
+let ved = 30
+
+// underscore 
+_delll = 20
+_19ji = 30
+
+// dont start a variable name with a number
+// 1 = 20
+
+// camel case 
+myNameVed = 10
+
+// Pascal case 
+MyNameVed = 10
+
+// snake case 
+my_Name_Ved = 10
+
+// Case Senstive : both are differnt  
+g = 10
+G = 20
+
+
+// create a variable with keyword 
+let y = 10
+// re-assignment 
+y = 20
+
+// let y = 100
+
+// keywords 
+// let 
+// 1. it ia a block scope.
+// 2. it cannot be re-decalred.
+
+
+{
+    let x = 10
+}
+// console.log(x)
+
+{
+    let x = 29
+}
+// var 
+// 1. it cannot be block scope.
+// 2. it can be re-decalred.
+
+var a = 10
+var a = 10
+
+// const
+// 1. it ia a block scope.
+// 2. it cannot be re-decalred.
+// 3. it cannot be re-assign
+
+const ab = 20
+ab = 50
+console.log(ab)
+
+
+
+
+
+
+
