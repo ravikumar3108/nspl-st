73ff40419f5275d0bf1804a84271a747b7990c9e
ravikumar <raviluhaniwal318@gmail.com>
2025-06-02 14:40:14 +0530

    op

--- Javascript/Vedparksh/index.js
+++ Javascript/Vedparksh/index.js
@@ -1,177 +1,222 @@
 
-console.log("External file")
+// console.log("External file")
 
-// Javascript is a programing language.It is scripted, interpted, precomplied and create dynamic features to web pages.
-// Complier = convert English language(coding language) to machine(laptop, computer) language. (Binary Number 0,1) 
-
-
-// let x = 10
-// let x = 20
-console.error("Error")
-console.warn("Warning")
-// alert("Alert On page")
+// // Javascript is a programing language.It is scripted, interpted, precomplied and create dynamic features to web pages.
+// // Complier = convert English language(coding language) to machine(laptop, computer) language. (Binary Number 0,1) 
 
 
-// Variables :- Variables are used to store our data/values.
-
-// create a variables 
-// x = 10
-// // x is variabel name 
-// // = is a assignment operator
-// // 10 is data 
-// console.log("x is",x)
-
-// variable names :- 
-// alphabets , names ,
-let s = 10
-let ved = 30
-
-// underscore 
-_delll = 20
-_19ji = 30
-
-// dont start a variable name with a number
-// 1 = 20
-
-// camel case 
-myNameVed = 10
-
-// Pascal case 
-MyNameVed = 10
-
-// snake case 
-my_Name_Ved = 10
-
-// Case Senstive : both are differnt  
-g = 10
-G = 20
+// // let x = 10
+// // let x = 20
+// console.error("Error")
+// console.warn("Warning")
+// // alert("Alert On page")
 
 
-// create a variable with keyword 
+// // Variables :- Variables are used to store our data/values.
+
+// // create a variables 
+// // x = 10
+// // // x is variabel name 
+// // // = is a assignment operator
+// // // 10 is data 
+// // console.log("x is",x)
+
+// // variable names :- 
+// // alphabets , names ,
+// let s = 10
+// let ved = 30
+
+// // underscore 
+// _delll = 20
+// _19ji = 30
+
+// // dont start a variable name with a number
+// // 1 = 20
+
+// // camel case 
+// myNameVed = 10
+
+// // Pascal case 
+// MyNameVed = 10
+
+// // snake case 
+// my_Name_Ved = 10
+
+// // Case Senstive : both are differnt  
+// g = 10
+// G = 20
+
+
+// // create a variable with keyword 
+// let y = 10
+// // re-assignment 
+// y = 20
+
+// // let y = 100
+
+// // keywords 
+// // let 
+// // 1. it ia a block scope.
+// // 2. it cannot be re-decalred.
+
+
+// {
+//     let x = 10
+// }
+// // console.log(x)
+
+// {
+//     let x = 29
+// }
+// // var 
+// // 1. it cannot be block scope.
+// // 2. it can be re-decalred.
+
+// var a = 10
+// var a = 10
+
+// // const
+// // 1. it ia a block scope.
+// // 2. it cannot be re-decalred.
+// // 3. it cannot be re-assign
+
+// const ab = 20
+// // ab = 50
+// // console.log(ab)
+
+
+
+// // Data Types :- 
+// // Primitive  : - single data store
+// // Non-primitive :- multiple data store
+
+
+
+// // Interger 
+// let num = 10
+// let num1 = 10.0
+
+// // String Data :- it is used to store Character values.
+// let str = "Ved"
+// let str1 = 'Ved'
+
+// // Boolean data ;- True and False 
+// let bl = true
+
+// // null 
+// let xs = null
+
+// // undefined 
+// let cv
+// console.log(cv)
+
+// // Array :- it is used to store multiple data types 
+
+// let arr = [1, 3, 4, 5, 6, 7, 8, 9, 9]
+// console.log(arr)
+// console.log(arr[2])
+
+// // object :- it is used to store multiple data types in the form of key and values
+
+// let obj = {
+//     name: "vedpraksh",
+//     age: 21,
+//     rollno: 34,
+//     classe: "12th"
+// }
+// console.log(obj)
+// console.log(obj.name)
+
+
+// // Operators : -
+// // Arithmetic Op
+// // + , - , * , / ,
+// // % Modulus = remainder
+// // **Expontation = power 
+// // ++ increment  +1 
+// // -- decremement  -1
+
+// let num3 = 13
+// let num5 = 13
+// let num4 = 4
+// console.log(num3 % num4)
+
+// console.log(num3 ** num4)
+
+// // Prefix 
+// let inc = ++num3
+// console.log(num3)
+// console.log(inc)
+
+// // postfix 
+// let inc1 = num5++
+// console.log(num5)
+// console.log(inc1)
+
+// // Assignment OP
+// // +=
+// // *=
+// // -=
+// // /=
+// // %=
+// // **=
+
+// let c = 10
+
+// let d = c + 10
+// c += 10
+// console.log(d)
+// console.log("c",c)
+
+// // Comparision OP  = true and false 
+// // ==  eqaul value check 
+// // === equal value and type 
+// // != vice versa of ==
+// // !== vice versa of === 
+// // > greater than 
+// // < lessa than 
+// // >= greater than equal to
+// // <= less than equal to
+
+
+
+// let x = 5
+// let y = "10"
+// let z = 10
+
+// console.log(x == y)
+// console.log(x != y)
+// console.log(x === y)
+// console.log(x !== y)
+
+// console.log(x >= z)
+
+// // // Logical OP
+// // or ||
+// // true || true = true 
+// // true || false = true
+// // false || true = true 
+// // false || false = false
+
+
+// // and && 
+// // true || true = true 
+// // true || false = false
+// // false || true = false 
+// // false || false = false
+
+// // not !
+
+// let x1 = !(x == y) 
+// console.log("x11",x1)
+
+
+let x = 500
 let y = 10
-// re-assignment 
-y = 20
+let z = 10
 
-// let y = 100
-
-// keywords 
-// let 
-// 1. it ia a block scope.
-// 2. it cannot be re-decalred.
-
-
-{
-    let x = 10
-}
-// console.log(x)
-
-{
-    let x = 29
-}
-// var 
-// 1. it cannot be block scope.
-// 2. it can be re-decalred.
-
-var a = 10
-var a = 10
-
-// const
-// 1. it ia a block scope.
-// 2. it cannot be re-decalred.
-// 3. it cannot be re-assign
-
-const ab = 20
-// ab = 50
-// console.log(ab)
-
-
-
-// Data Types :- 
-// Primitive  : - single data store
-// Non-primitive :- multiple data store
-
-
-
-// Interger 
-let num = 10
-let num1 = 10.0
-
-// String Data :- it is used to store Character values.
-let str = "Ved"
-let str1 = 'Ved'
-
-// Boolean data ;- True and False 
-let bl = true
-
-// null 
-let xs = null
-
-// undefined 
-let cv
-console.log(cv)
-
-// Array :- it is used to store multiple data types 
-
-let arr = [1, 3, 4, 5, 6, 7, 8, 9, 9]
-console.log(arr)
-console.log(arr[2])
-
-// object :- it is used to store multiple data types in the form of key and values
-
-let obj = {
-    name: "vedpraksh",
-    age: 21,
-    rollno: 34,
-    classe: "12th"
-}
-console.log(obj)
-console.log(obj.name)
-
-
-// Operators : -
-// Arithmetic Op
-// + , - , * , / ,
-// % Modulus = remainder
-// **Expontation = power 
-// ++ increment  +1 
-// -- decremement  -1
-
-let num3 = 13
-let num5 = 13
-let num4 = 4
-console.log(num3 % num4)
-
-console.log(num3 ** num4)
-
-// Prefix 
-let inc = ++num3
-console.log(num3)
-console.log(inc)
-
-// postfix 
-let inc1 = num5++
-console.log(num5)
-console.log(inc1)
-
-// Assignment OP
-// +=
-// *=
-// -=
-// /=
-// %=
-// **=
-
-let c = 10
-
-let d = c + 10
-c += 10
-console.log(d)
-console.log("c",c)
-
-// Comparision OP
-// Logical OP
-
-
+let marks = (x >= 60 || y >= 70) && y == z
+console.log(marks)
 
 
 
