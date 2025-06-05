dc21a0622fe619fbb3afe50e95c3d539d3b90ac0
ravikumar <raviluhaniwal318@gmail.com>
2025-06-05 14:23:17 +0530

    loops

---
+++ Javascript/Vedparksh/Loops.js
@@ -0,0 +1,33 @@
+
+
+// Loops :- it is a statement which is run when our condition is as well as true.
+
+// while loop 
+//  do while loop === one time run on false result 
+//  for loop
+
+// initialise 
+// condition 
+// increment / decrement  
+
+let x = 0
+
+
+// while (x >= 20) {
+//     console.log(x)
+//     x++
+// }
+
+do {
+    console.log(x)
+    x++
+} while (x > 10)
+
+
+for(let y = 1; y < 10; y++){
+    console.log(y)
+}
+
+// table 
+
+
