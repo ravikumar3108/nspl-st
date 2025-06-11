1d9fe35dcb5916f6ffd4dee2dff5dda9851b8721
ravikumar <raviluhaniwal318@gmail.com>
2025-06-11 14:53:16 +0530

    string

---
+++ Javascript/Vedparksh/String.js
@@ -0,0 +1,45 @@
+
+// String :- use string type to store the charcter values. 
+// string works on index number 
+// string count whitespaces.
+// String is immutable 
+
+
+let str =  "Hello my my name is Ved."
+
+console.log(str.length)
+// Access the string data 
+
+console.log(str[5])
+
+// replace 
+// replace all
+let st1 = str.replaceAll("my" , "Hii")
+console.log(st1)
+
+// toUpperCase
+// toLowerCase
+
+let l = str.toLowerCase()
+console.log(l)
+let p = str.toUpperCase()
+console.log(p)
+
+// slice
+// concat 
+// trim :- remove the whitespaces at the begining and the ending.
+
+let str1 = "             helooo            "
+console.log(str1.length)
+let t = str1.trim()
+console.log(str1)
+console.log(t)
+console.log(t.length)
+
+// trimStart
+// trimEnd 
+
+console.log(l.at(2))
+
+
+
