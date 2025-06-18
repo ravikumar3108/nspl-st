c0421612b645c2daf65592794de9cb186a9a6879
ravikumar <raviluhaniwal318@gmail.com>
2025-06-18 14:29:22 +0530

    fun

---
+++ React JS/ved/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from "./components/Home"
+
+function App() {
+  return (
+    // JSX Fragments 
+    <>
+      {/* Call a function components */}
+
+      <Home />
+      <Home />
+      <Home />
+      <Home />
+      <Home />
+      <Home />
+      <Home />
+      <Home />
+      <Home />
+
+    </>
+  );
+}
+
+export default App;
---
+++ React JS/ved/src/components/Home.jsx
@@ -0,0 +1,18 @@
+//  create a function component
+
+import React from "react";
+
+function Home() {
+
+    let x = 10
+
+
+  return (
+    <>
+      <h1>Helooo {x*10}</h1>
+      <p>Ved</p>
+    </>
+  );
+}
+
+export default Home
