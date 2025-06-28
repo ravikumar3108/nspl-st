b45b1630ce93d0c2eb0f5ad80cbe706b0e69b197
ravikumar <raviluhaniwal318@gmail.com>
2025-06-28 10:02:21 +0530

    web

--- React JS/harish/src/components/Api.jsx
+++ React JS/harish/src/components/Api.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Api() {
-  let [apiData, setApiData] = useState();
+  let [apiData, setApiData] = useState([]);
   console.log(apiData);
 
   // Usenavigate
@@ -10,18 +10,41 @@ function Api() {
   // fetch
   // Asynchornus function = async and await // promises
   // callback function
+  // UseEffect = Side effect of our component
 
+  
   async function getData() {
     const data = await fetch("https://dummyjson.com/products");
     const jsondata = await data.json();
     setApiData(jsondata.products);
   }
+  
+  // Arrow Function => ()=>
+    
+    useEffect(()=>{
+      getData()
+    },[])
+
 
   return (
     <div>
       <button onClick={getData}>Fecth Api</button>
 
       {/* Array method map  */}
+      {/* arrow function use as a Callback function  */}
+      <div className="main">
+        {apiData.map((item) => {
+          return (
+            <>
+             <div className="item">
+               <h1> {item.id}</h1>
+               <img src={item.images} alt="" />
+               <p>{item.description}</p>
+             </div>
+            </>
+          );
+        })}
+      </div>
     </div>
   );
 }
