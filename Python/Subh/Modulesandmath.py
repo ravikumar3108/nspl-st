cd66f1aa7aa5e651ac647c533ce069e0e6b04721
ravikumar <raviluhaniwal318@gmail.com>
2025-06-30 16:49:09 +0530

    flex

---
+++ Python/Subh/Modulesandmath.py
@@ -0,0 +1,33 @@
+#  Modules :- 
+# math and in bulits fucntions 
+# 
+#  
+
+# max 
+# min 
+# abs = absolute = =+ve data
+# pow = power 
+
+x = [1,2,4,6,7]
+print(max(x)) 
+
+x = 2
+# expontation 2 ** 20
+print(pow(x,20))
+
+# date 
+import datetime
+
+x = datetime.datetime.now()
+print(x)
+
+import math 
+y = math.pi 
+print(y)
+
+
+y1 = math.ceil(2.4) # upward 
+y2 = math.floor(2.4) #lowest value 
+
+print("y1",y1)
+print("y2",y2)
