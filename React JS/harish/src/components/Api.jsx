32ea7e7b97bb8c831b8c82baf5e8a7ec39326b3a
ravikumar <raviluhaniwal318@gmail.com>
2025-07-01 09:47:33 +0530

    filter data in raect

--- React JS/harish/src/components/Api.jsx
+++ React JS/harish/src/components/Api.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 
 function Api() {
   let [apiData, setApiData] = useState([]);
-  console.log(apiData);
+  let [filterDD, setFilterDD] = useState([]);
 
   // Usenavigate
   // Toast
@@ -12,35 +12,58 @@ function Api() {
   // callback function
   // UseEffect = Side effect of our component
 
-  
   async function getData() {
     const data = await fetch("https://dummyjson.com/products");
     const jsondata = await data.json();
     setApiData(jsondata.products);
+    setFilterDD(jsondata.products)
   }
-  
-  // Arrow Function => ()=>
-    
-    useEffect(()=>{
-      getData()
-    },[])
 
+  const filterItem = (category) => {
+    const filterData = apiData.filter((item) => item.category == category);
+    setFilterDD(filterData)
+  };
+
+  // Arrow Function => ()=>
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <div>
-      <button onClick={getData}>Fecth Api</button>
+      <button className="btn" onClick={() => {getData()}}>
+        All
+      </button>
+      <button onClick={() => filterItem("beauty")} className="btn">
+        Beauty
+      </button>
+      <button onClick={() => filterItem("fragrances")} className="btn">
+        Frangnance
+      </button>
+      <button onClick={getData} className="btn">
+        groceries
+      </button>
+      <button onClick={getData} className="btn">
+        Furniture
+      </button>
 
       {/* Array method map  */}
       {/* arrow function use as a Callback function  */}
-      <div className="main">
-        {apiData.map((item) => {
+      <div className="main" style={{ display: "flex", flexWrap: "wrap" }}>
+        {filterDD.map((item) => {
           return (
             <>
-             <div className="item">
-               <h1> {item.id}</h1>
-               <img src={item.images} alt="" />
-               <p>{item.description}</p>
-             </div>
+              <div className="item" style={{ width: "33%" }}>
+                <h1> {item.id}</h1>
+                <div
+                  style={{
+                    width: "10%",
+                  }}
+                >
+                  <img src={item.images} alt="" width={"100%"} />
+                </div>
+                <p>{item.category}</p>
+              </div>
             </>
           );
         })}
