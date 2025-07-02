4117d38d6c217980cdfa8eb569dd7aadfc9aa2a5
ravikumar <raviluhaniwal318@gmail.com>
2025-07-02 16:27:13 +0530

    js

---
+++ Hriday/Javascript/index.js
@@ -0,0 +1,72 @@
+// //  create a varibale 
+// // Declare a variable 
+// x = 10
+// // / KeyWords
+// // let 
+// // var 
+
+// let y = 10
+// console.log(y)
+// // re-assignment 
+// y = 20
+// console.log(y)
+
+// // {} = block scope
+// {
+//     let h = 10
+//     console.log(h)
+
+// }
+
+// const k = 10
+
+// // alphabets , name 
+// // alpha + number 
+// x10 = 30
+// // underscore
+// _iiii
+// // Camel case 
+// myVariableFirst;
+// // PAscal Case 
+// MyVariable
+// // Snake Case 
+// my_variale_
+
+
+// Data Types '
+//1... Primitive Data types:- Single data store
+// String "" ''
+let x = "Hriday"
+let y = null
+// Number
+// Bigint
+// Boolean = true and false 
+// Undefined = 
+// Null 
+let c;
+console.log(c)
+
+
+//2..... Non-primitive
+// Array
+let arr = [1, 2, 4, 7, 89, 0, 0, "Hhh", "nukk", true, false, null]
+
+// Object = key and value pair 
+let obj = {
+    name: "Hriday",
+    classe: "12th",
+    Rollno: 12
+}
+
+console.log(obj.name)
+
+xx11 = 10
+xx11 += 10
+https://www.w3schools.com/js/js_assignment.asp
+
+
+
+
+
+
+
