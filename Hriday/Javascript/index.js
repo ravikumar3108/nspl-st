16be2b5819a1821d3b7788a86f7a186827519c96
ravikumar <raviluhaniwal318@gmail.com>
2025-07-03 15:07:35 +0530

    js

--- Hriday/Javascript/index.js
+++ Hriday/Javascript/index.js
@@ -1,72 +1,117 @@
-// //  create a varibale 
-// // Declare a variable 
-// x = 10
-// // / KeyWords
-// // let 
-// // var 
+// // //  create a varibale
+// // // Declare a variable
+// // x = 10
+// // // / KeyWords
+// // // let
+// // // var
 
-// let y = 10
-// console.log(y)
-// // re-assignment 
-// y = 20
-// console.log(y)
+// // let y = 10
+// // console.log(y)
+// // // re-assignment
+// // y = 20
+// // console.log(y)
 
-// // {} = block scope
-// {
-//     let h = 10
-//     console.log(h)
+// // // {} = block scope
+// // {
+// //     let h = 10
+// //     console.log(h)
+
+// // }
+
+// // const k = 10
+
+// // // alphabets , name
+// // // alpha + number
+// // x10 = 30
+// // // underscore
+// // _iiii
+// // // Camel case
+// // myVariableFirst;
+// // // PAscal Case
+// // MyVariable
+// // // Snake Case
+// // my_variale_
+
+
+// // Data Types '
+// //1... Primitive Data types:- Single data store
+// // String "" ''
+// let x = "Hriday"
+// let y = null
+// // Number
+// // Bigint
+// // Boolean = true and false
+// // Undefined =
+// // Null
+// let c;
+// console.log(c)
+
+
+// //2..... Non-primitive
+// // Array
+// let arr = [1, 2, 4, 7, 89, 0, 0, "Hhh", "nukk", true, false, null]
+
+// // Object = key and value pair
+// let obj = {
+//     name: "Hriday",
+//     classe: "12th",
+//     Rollno: 12
+// }
+
+// console.log(obj.name)
+
+// xx11 = 10
+// xx11 += 10
+// https://www.w3schools.com/js/js_assignment.asp
+
+
+
+// Javascript Arithmetic And Assignment Op
+// Javascript Comparision And Logical Op
+
+
+// If else statements
+// // Conditional Statements
+// // Very often when you write code, you want to perform different actions for different decisions.
+
+// // You can use conditional statements in your code to do this.
+
+// // In JavaScript we have the following conditional statements:
+
+// // Use if to specify a block of code to be executed, if a specified condition is true
+// // Use else to specify a block of code to be executed, if the same condition is false
+// // Use else if to specify a new condition to test, if the first condition is false
+// // Use switch to specify many alternative blocks of code to be executed
+
+
+// if (condition) {
+
+// } else {
 
 // }
 
-// const k = 10
-
-// // alphabets , name 
-// // alpha + number 
-// x10 = 30
-// // underscore
-// _iiii
-// // Camel case 
-// myVariableFirst;
-// // PAscal Case 
-// MyVariable
-// // Snake Case 
-// my_variale_
 
 
-// Data Types '
-//1... Primitive Data types:- Single data store
-// String "" ''
-let x = "Hriday"
-let y = null
-// Number
-// Bigint
-// Boolean = true and false 
-// Undefined = 
-// Null 
-let c;
-console.log(c)
+// if (condition) {
+
+// } else if (condition) {
+
+// }
+// else {
+
+// }
 
 
-//2..... Non-primitive
-// Array
-let arr = [1, 2, 4, 7, 89, 0, 0, "Hhh", "nukk", true, false, null]
 
-// Object = key and value pair 
-let obj = {
-    name: "Hriday",
-    classe: "12th",
-    Rollno: 12
+let x = 8
+if(x > 10){
+    console.log("X is greater than 10")
+}else if(x < 10){
+ console.log("X is smaller than 10")
 }
-
-console.log(obj.name)
-
-xx11 = 10
-xx11 += 10
-https://www.w3schools.com/js/js_assignment.asp
-
-
-
-
-
-
-
+else if (x == 10){
+    
+}
+else{
+    
+}
