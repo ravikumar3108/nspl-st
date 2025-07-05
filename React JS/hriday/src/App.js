244dae1c5aafa1c6b3efc811a89995f9bfcf1d49
ravikumar <raviluhaniwal318@gmail.com>
2025-07-05 14:58:40 +0530

    react

---
+++ React JS/hriday/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Navbar from "./components/Utility/Navbar";
+
+function App() {
+  return (
+    <div>
+      {/* Call the function component */}
+      <Navbar/>
+      <Navbar/>
+      <Navbar/>
+      <Navbar/>
+      <Navbar/>
+      <Navbar/>
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ React JS/hriday/src/components/Utility/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    // JSX fragments
+    <>
+      <h1
+        className="header"
+        id=""
+        style={{
+          color: "red",
+          backgroundColor: "black",
+        }}
+      >
+        Navbar
+      </h1>
+    </>
+  );
+}
+
+export default Navbar;
