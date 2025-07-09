f9b836f9310965f114e71ef333877c62852dff64
ravikumar <raviluhaniwal318@gmail.com>
2025-07-09 14:41:17 +0530

    react native

---
+++ React Native/Ved/my-app/app/(tabs)/explore.tsx
@@ -0,0 +1,19 @@
+import { Platform, StyleSheet, Text, View } from 'react-native';
+
+
+export default function TabTwoScreen() {
+  return (
+   <>
+   <View>
+      <Text style={styles.text}>Helooo Vedparkash</Text>
+   </View>
+   </>
+  );
+}
+
+
+const styles = StyleSheet.create({
+    text :{
+        color:"red",
+    }
+})
