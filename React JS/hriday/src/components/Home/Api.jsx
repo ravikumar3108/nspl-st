8feac217c7822548d4537cb61b94a4ac0137e3c2
ravikumar <raviluhaniwal318@gmail.com>
2025-07-11 15:56:12 +0530

    filter

--- React JS/hriday/src/components/Home/Api.jsx
+++ React JS/hriday/src/components/Home/Api.jsx
@@ -1,45 +1,51 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 function Api() {
+  let [apiData, setApiData] = useState([]);
+  let [filterDD, setFilterDD] = useState([]);
 
-    let [apiData,setApiData] = useState([])
-    console.log(apiData)
+  // Async await = asynchronous = promises
+  async function getData() {
+    let dataa = await fetch("https://dummyjson.com/products");
+    let jsondata = await dataa.json();
+    // console.log(jsondata.products)
+    setApiData(jsondata.products);
+    setFilterDD(jsondata.products)
+  }
 
-    // Async await = asynchronous = promises 
-   async function getData(){
-        let dataa = await fetch("https://dummyjson.com/products")
-        let jsondata = await dataa.json()
-        // console.log(jsondata.products)
-        setApiData(jsondata.products)
-    }
+  // ()=> arrow function
 
-    // ()=> arrow function
+  function FilterDataApi(category) {
+    let filterData = apiData.filter((item)=>item.category == category)
+    setFilterDD(filterData)
+  }
 
-    useEffect(()=>{
-        getData()
-    },[])
+  useEffect(() => {
+    getData();
+  }, []);
 
-    return (
+  return (
     <>
-        <h1>
-
-        </h1>
-        {/* <button onClick={getData}>GEtdataa</button> */}
-        {apiData.map((item)=>{
-            return(
-                <>
-                <h3>{item.id}</h3>
-                <Link to={`more?id=${item.id}`}>{item.title}</Link>
-                <p>{item.description}</p>
-                <p>{item.price}</p>
-                <img src="" alt="" />
-                </>
-            )
-        })}
-
+      <h1></h1>
+      <button onClick={getData}>All</button>
+      <button onClick={()=>FilterDataApi("beauty")}>Beauty</button>
+      <button onClick={()=>FilterDataApi("fragrances")}>Fragnance</button>
+      <button onClick={()=>FilterDataApi("groceries")}>groceries</button>
+      {/* <button onClick={getData}>GEtdataa</button> */}
+      {filterDD.map((item) => {
+        return (
+          <>
+            <h3>{item.id}</h3>
+            <Link to={`more?id=${item.id}`}>{item.title}</Link>
+            <p>{item.description}</p>
+            <p>{item.price}</p>
+            <img src="" alt="" />
+          </>
+        );
+      })}
     </>
-  )
+  );
 }
 
-export default Api
+export default Api;
