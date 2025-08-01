13e38f46059c1fb18a6c2118e687561df867a414
ravikumar <raviluhaniwal318@gmail.com>
2025-08-01 14:53:27 +0530

    flatlist and scrolview

--- React Native/Ved/my-app/app/(tabs)/explore.tsx
+++ React Native/Ved/my-app/app/(tabs)/explore.tsx
@@ -1,13 +1,41 @@
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { FlatList, Platform, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 
 export default function TabTwoScreen() {
+
+  const Users = [
+    {id:1,name:"Ravi Kumar",Designation:"Trainer"},
+    {id:2,name:"ved",Designation:"Student"},
+    {id:3,name:"Parkash",Designation:"Student"},
+    {id:4,name:"Ravi",Designation:"Student"},
+  ]  
+
   return (
-   <>
-   <View>
-      <Text style={styles.text}>Helooo Vedparkash</Text>
-   </View>
-   </>
+   <SafeAreaView style={styles.main}>
+    <FlatList 
+      data={Users}
+      renderItem={({item})=>(
+        <View style={styles.mainbox}>
+          <Text style={styles.color}>{item.Designation}</Text>
+          <Text style={styles.color}>{item.name}</Text>
+        </View>
+      )}
+      extraData={""}
+      // numColumns={2}
+    />
+    {/* <ScrollView horizontal >
+        <View style={styles.box}></View>
+        <View style={styles.boxgreen}></View>
+        <View style={styles.box}></View>
+        <View style={styles.boxgreen}></View>
+        <View style={styles.box}></View>
+        <View style={styles.boxgreen}></View>
+        <View style={styles.box}></View>
+        <View style={styles.boxgreen}></View>
+        <View style={styles.box}></View>
+        <View style={styles.boxgreen}></View>
+    </ScrollView> */}
+   </SafeAreaView>
   );
 }
 
@@ -15,5 +43,36 @@ export default function TabTwoScreen() {
 const styles = StyleSheet.create({
     text :{
         color:"red",
+    },
+    box:{
+      width:100,
+      height:100,
+      borderWidth:1,
+      backgroundColor:"red",
+      borderRadius:50
+    },
+    boxgreen:{
+      width:100,
+      height:100,
+      borderWidth:1,
+      backgroundColor:"green",
+      borderRadius:50,
+      gap:10,
+    },
+    color:{
+      color:"white",
+      textAlign:"center"
+     
+    },
+    main:{
+      flex:1
+    },
+    mainbox:{
+      borderWidth:1,
+      width:100,
+      height:100,
+      backgroundColor:"blue",
+      paddingTop:20
     }
+
 })
--- React Native/Ved/my-app/app/(tabs)/index.tsx
+++ React Native/Ved/my-app/app/(tabs)/index.tsx
@@ -1,75 +1,57 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { Platform, SafeAreaView, StyleSheet ,Text,TextInput,TouchableOpacity,View } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
+
+  const [data,setData] = useState("")
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+   <>
+   <SafeAreaView style={{flex:1}}>
+     {/* <View style={styles.main}>
+      <Text style={styles.header}>Login</Text>
+      <TextInput style={styles.input} onChangeText={setData} placeholder='Username'/>
+      <TextInput style={styles.input} placeholder='Password'/>
+
+      <TouchableOpacity>
+        <Text style={styles.btn}>Submit Button</Text>
+      </TouchableOpacity>
+    </View> */}
+
+    
+   </SafeAreaView>
+   </>
   );
 }
 
+
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+   main:{
+      alignContent:"center",
+      margin:30,
+    },
+    input :{
+        color:"red",
+        padding:20,
+        height:20,
+        marginBottom:20,
+        borderWidth:1
+    },
+    header:{
+      textAlign:"center",
+      fontSize:30,
+      margin:20
+    },
+    btn:{
+      borderWidth:1,
+      padding:10,
+      borderRadius:10,
+      backgroundColor:"blue",
+      color:"white",
+      textAlign:"center"
+    }
+   
+
+})
