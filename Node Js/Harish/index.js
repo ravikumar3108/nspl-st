b81d583786ec85b61a1d0bd1c2f64cdd6d34ca2d
ravikumar <raviluhaniwal318@gmail.com>
2025-08-06 09:59:22 +0530

    node js

--- Node Js/Harish/index.js
+++ Node Js/Harish/index.js
@@ -2,6 +2,25 @@ const express = require('express')
 const app = express()
 const usersRoutes = require("./routes/UserRoutes")
 
+const mongoose = require('mongoose');
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+}
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email:String,
+  password:String
+});
+
+
+const users = mongoose.model('Userdata', userSchema);
+
+
 
 // Middlewares 
 app.use(express.json())
