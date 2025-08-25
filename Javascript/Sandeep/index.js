0ec6d93dab260fc161d8a78a235416ce7d78848c
ravikumar <raviluhaniwal318@gmail.com>
2025-08-25 11:25:03 +0530

    js

---
+++ Javascript/Sandeep/index.js
@@ -0,0 +1,20 @@
+// JavaScript is the programming language of the web.
+
+// It can update and change both HTML and CSS.
+
+// It can calculate, manipulate and validate data.
+
+// Output Function :- Console.log()
+
+// Javascript is a scripted , interpted and precompiled language.
+
+// Interpted :- which means reads the code line by line and executed.
+// compiler :- it is used to convert our code into machine language.(0,1) Binary numbers
+
+
+console.log("external file code")
+console.warn("Warning")
+console.error("Error")
+
+
+
