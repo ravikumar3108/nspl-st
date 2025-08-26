e29157f716132f9968e62e2fa2a9eec775156c93
ravikumar <raviluhaniwal318@gmail.com>
2025-08-26 11:41:23 +0530

    variables

--- Javascript/Sandeep/index.js
+++ Javascript/Sandeep/index.js
@@ -17,4 +17,52 @@ console.warn("Warning")
 console.error("Error")
 
 
+// Variables :- Variables are used to store our data.
+
+// Syntax =
+// Keywords = Var , let , const 
+
+// Create a variable
+// x = varibale name
+// = => assignment operator 
+// 10 = integer data
+let x = 10
+console.log(x)
+
+
+// Variables names :- Identifiers 
+// 1. Alphabets / capital and small / names 
+let sandeep = 30
+console.log(sandeep)
+// 2. Alphabets + numbers
+let x10 = 30
+let sandeep20 = 30
+
+// 3. underscore
+let _under = 30
+let sandeep_kumar = 30
+// 4. Camel case
+let sandeepKumar = 30
+// 5. pascal case
+let SandeepKumar = 20
+// 6. Not a number 
+// let 10 = 10
+
+
+// Let :- it cannot be redeclared :- 
+let z = 10
+// let z = 20
+console.log("z is", z)
+
+// Re-assign 
+z = 30
+console.log("z is", z)
+
+// constant = it cannot be redecalred
+const a = 10
+// re-assign :- re-assign is not supported.
+a = 30
+console.log(a)
+
+
 
