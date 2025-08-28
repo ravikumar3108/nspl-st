1970caa6c8d10f823a00da681fda141584505e56
ravikumar <raviluhaniwal318@gmail.com>
2025-08-28 10:00:06 +0530

    login

--- Node Js/Harish/controllers/UserControllers.js
+++ Node Js/Harish/controllers/UserControllers.js
@@ -2,23 +2,31 @@ const express = require('express')
 const User = require("../models/UserModel")
 
 const userLogin = async (req, res) => {
-    console.log(req.body)
-    const newuser = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-    })
+    try {
+        const newuser = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        })
 
-    const saveuser = await newuser.save()
-    res.json({ message: saveuser })
+        const saveuser = await newuser.save()
+
+        if (saveuser) {
+            res.json({ message: saveuser, status: true })
+        } else {
+            res.json({ message: saveuser, status: false })
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 const getUser = async (req, res) => {
     console.log(req.body)
-    // const alldata = await User.find({})
-    const alldata = await User.findOne({name: "Harish",})
+    
+    const alldata = await User.findOne({email : req.body.email})
     res.json({ message: alldata })
-
 }
 
 module.exports = { userLogin, getUser }
