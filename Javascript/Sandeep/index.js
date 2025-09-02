05e81f5f3ade086bf7cf86c60fe3736e5d2dce98
ravikumar <raviluhaniwal318@gmail.com>
2025-09-02 11:22:44 +0530

    op

--- Javascript/Sandeep/index.js
+++ Javascript/Sandeep/index.js
@@ -1,192 +1,228 @@
-// // JavaScript is the programming language of the web.
+// // // JavaScript is the programming language of the web.
 
-// // It can update and change both HTML and CSS.
+// // // It can update and change both HTML and CSS.
 
-// // It can calculate, manipulate and validate data.
+// // // It can calculate, manipulate and validate data.
 
-// // Output Function :- Console.log()
+// // // Output Function :- Console.log()
 
-// // Javascript is a scripted , interpted and precompiled language.
+// // // Javascript is a scripted , interpted and precompiled language.
 
-// // Interpted :- which means reads the code line by line and executed.
-// // compiler :- it is used to convert our code into machine language.(0,1) Binary numbers
-
-
-// console.log("external file code")
-// console.warn("Warning")
-// console.error("Error")
+// // // Interpted :- which means reads the code line by line and executed.
+// // // compiler :- it is used to convert our code into machine language.(0,1) Binary numbers
 
 
-// // Variables :- Variables are used to store our data.
+// // console.log("external file code")
+// // console.warn("Warning")
+// // console.error("Error")
 
-// // Syntax =
-// // Keywords = Var , let , const 
 
-// // Create a variable
-// // x = varibale name
-// // = => assignment operator 
-// // 10 = integer data
-// let x = 10
+// // // Variables :- Variables are used to store our data.
+
+// // // Syntax =
+// // // Keywords = Var , let , const 
+
+// // // Create a variable
+// // // x = varibale name
+// // // = => assignment operator 
+// // // 10 = integer data
+// // let x = 10
+// // console.log(x)
+
+
+// // // Variables names :- Identifiers 
+// // // 1. Alphabets / capital and small / names 
+// // let sandeep = 30
+// // console.log(sandeep)
+// // // 2. Alphabets + numbers
+// // let x10 = 30
+// // let sandeep20 = 30
+
+// // // 3. underscore
+// // let _under = 30
+// // let sandeep_kumar = 30
+// // // 4. Camel case
+// // let sandeepKumar = 30
+// // // 5. pascal case
+// // let SandeepKumar = 20
+// // // 6. Not a number 
+// // // let 10 = 10
+
+
+// // // Let :- it cannot be redeclared :- 
+// // let z = 10
+// // // let z = 20
+// // console.log("z is", z)
+
+// // // Re-assign 
+// // z = 30
+// // console.log("z is", z)
+
+// // // constant = it cannot be redecalred
+// // const a = 10
+// // // re-assign :- re-assign is not supported.
+// // // a = 30
+// // // console.log(a)
+
+
+// // // Data Types:-
+// // // 1. Integer
+
+// // let num1 = 20
+// // let num2 = 40
+// // console.log(num1 + num2)
+
+// // // 2. String : - string is used to store character(alphabets) values.
+// // // String data store into single quotes and double quotes "" ''
+
+// // let name = "kumar"
+// // let name1 = 'kumar'
+// // let numchar = "5"
+
+
+// // // 3. bollean
+// // let bl1 = true
+// // let bl2 = false
+
+// // // 4. null
+
+// // let nl = null
+
+// // // 5. undefined
+// // let un;
+// // console.log(un)
+
+// // // 6. array :- Array is used to store multiple data/values
+
+// // let arr = [1, 2, 4, 5, 6, 8, 9, "Name", null, undefined, true, false]
+// // console.log(arr)
+
+// // // 7. object : object is used to store multiple data/values in the form of key and values
+
+// // let obj = {
+// //     id: 1,
+// //     name: "Sandeep",
+// //     rollno: 21,
+// //     classe: "12th"
+// // }
+
+// // console.log(obj)
+// // console.log(obj.name)
+// // console.log(obj.classe)
+
+
+// // Javascript Operators :- 
+// // 1. Arithmetic operators
+// // + , - , * , / ,
+
+// let num1 = 90
+// let num2 = 20
+// let num3 = "10"
+// let num4 = "10"
+
+// let sum = num1 + num2
+// console.log("num1 + num2 is :", sum + 100)
+// console.log("num1 + num3 is :", num1 + num3)
+
+// let x = 15
+// let y = 3
+
+// // console.log(x ** y)
+// console.log(x % y)
+
+// console.log(x++)
 // console.log(x)
+// console.log(--y)
+// //  ** => expontatioon (Power)
+// // % => modulus (remainder)
+// // ++ => increment op
+// // -- => decrement op
 
 
-// // Variables names :- Identifiers 
-// // 1. Alphabets / capital and small / names 
-// let sandeep = 30
-// console.log(sandeep)
-// // 2. Alphabets + numbers
-// let x10 = 30
-// let sandeep20 = 30
 
-// // 3. underscore
-// let _under = 30
-// let sandeep_kumar = 30
-// // 4. Camel case
-// let sandeepKumar = 30
-// // 5. pascal case
-// let SandeepKumar = 20
-// // 6. Not a number 
-// // let 10 = 10
+// // 2. Assignment op
+// // = 
+// // += 
+// let a = 10
+// let b = 20
+// let c = a + b
+// a += 20
+// console.log(a)
+// console.log(c)
 
+// // -=
+// // *=
+// // /=
+// // %=
+// // **= 
 
-// // Let :- it cannot be redeclared :- 
-// let z = 10
-// // let z = 20
-// console.log("z is", z)
+// // 3. Comparision op = (true and false)
+// // == equal value
+// // != not equal
+// // === equal value and equal type
+// // !== not equal value and type
+// // > greater than
+// // < less than 
+// // >= greater than equal to
+// // <=  less than equal to
 
-// // Re-assign 
-// z = 30
-// console.log("z is", z)
+// let aa = 12
+// let bb = 13
+// let cc = 12
+// let dd = "12"
 
-// // constant = it cannot be redecalred
-// const a = 10
-// // re-assign :- re-assign is not supported.
-// // a = 30
-// // console.log(a)
+// // console.log(aa == bb)
+// // console.log(aa == cc)
+// // console.log(aa == dd)
+// // console.log(aa != cc) // opposite ==
 
-
-// // Data Types:-
-// // 1. Integer
-
-// let num1 = 20
-// let num2 = 40
-// console.log(num1 + num2)
-
-// // 2. String : - string is used to store character(alphabets) values.
-// // String data store into single quotes and double quotes "" ''
-
-// let name = "kumar"
-// let name1 = 'kumar'
-// let numchar = "5"
+// // console.log(aa === bb)
+// // console.log(aa === dd)
 
 
-// // 3. bollean
-// let bl1 = true
-// let bl2 = false
-
-// // 4. null
-
-// let nl = null
-
-// // 5. undefined
-// let un;
-// console.log(un)
-
-// // 6. array :- Array is used to store multiple data/values
-
-// let arr = [1, 2, 4, 5, 6, 8, 9, "Name", null, undefined, true, false]
-// console.log(arr)
-
-// // 7. object : object is used to store multiple data/values in the form of key and values
-
-// let obj = {
-//     id: 1,
-//     name: "Sandeep",
-//     rollno: 21,
-//     classe: "12th"
-// }
-
-// console.log(obj)
-// console.log(obj.name)
-// console.log(obj.classe)
+// console.log(aa > bb)
+// console.log(aa < bb)
+// console.log(aa <= cc)
 
 
-// Javascript Operators :- 
-// 1. Arithmetic operators
-// + , - , * , / ,
 
-let num1 = 90
+//     // 4. Logical op
+
+//     // && and
+//     // true && true = true 
+//     // true && false = false 
+//     // false && true = false 
+//     // false && false = false
+
+
+//     // || or
+// // true || true = true 
+// // true || false = true 
+// // false || true = true 
+// // false || false = false
+
+// // ! not
+
+let num1 = 10
 let num2 = 20
-let num3 = "10"
-let num4 = "10"
+let num3 = 20
 
-let sum = num1 + num2
-console.log("num1 + num2 is :", sum + 100)
-console.log("num1 + num3 is :", num1 + num3)
-
-let x = 15
-let y = 3
-
-// console.log(x ** y)
-console.log(x % y)
-
-console.log(x++)
-console.log(x)
-console.log(--y)
-//  ** => expontatioon (Power)
-// % => modulus (remainder)
-// ++ => increment op
-// -- => decrement op
+let cc = num1 > num2 && num3 == num2
+let dd = !(num1 > num2 || num3 == num2)
+console.log(cc)
+console.log(dd)
 
 
+// If else Statements (conditional statements):- it is a block of code which is executed when our condition is true
 
-// 2. Assignment op
-// = 
-// += 
-let a = 10
-let b = 20
-let c = a + b
-a += 20
-console.log(a)
-console.log(c)
+// Syntax :-
 
-// -=
-// *=
-// /=
-// %=
-// **= 
+// if(condition){
 
-// 3. Comparision op = (true and false)
-// == equal value
-// != not equal
-// === equal value and equal type
-// !== not equal value and type
-// > greater than
-// < less than 
-// >= greater than equal to
-// <=  less than equal to
-
-let aa = 12
-let bb = 13
-let cc = 12
-let dd = "12"
-
-// console.log(aa == bb)
-// console.log(aa == cc)
-// console.log(aa == dd)
-// console.log(aa != cc) // opposite ==
-
-// console.log(aa === bb)
-// console.log(aa === dd)
-
-
-console.log(aa > bb)
-console.log(aa < bb)
-console.log(aa <= cc)
-
-
-
-// 4. Logical op
+// }
+// else{
+// default condition
+// }
 
 
 
