83d180174eb9d54106be4cb4304875f4dd09e34f
Ravi Kumar <raviluhaniwal318@gmail.com>
2025-11-06 11:17:58 +0530

    form submit

--- React JS/sandeep/src/components/Form.jsx
+++ React JS/sandeep/src/components/Form.jsx
@@ -16,17 +16,48 @@ function Form() {
         setEmail(e.target.value)
     }
 
+    let [allData, setAllData] = useState()
+    console.log("alldata",allData)
+
+    function getAllData(e){
+        setAllData({
+            // spread operator 
+            ...allData,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    // Json Object :- javascript object notation
+
+    // let obj = {
+    //     "name":"sandeep",
+    //     "id":1
+    // }
+
+
+    function handleSubmit(e){
+        e.preventDefault()
+        console.log("submit function call")
+        localStorage.setItem("users",JSON.stringify(allData));
+        
+    }
+
+
   return (
     <>
-
-        <form action="">
+        <form action="" onSubmit={handleSubmit}>
             <label htmlFor="">FirstName</label> <br />
-            <input type="text" name="" id="" onChange={getFirstName}/> <br /> <br />
+            <input type="text" name="firstname" id="" onChange={getAllData}/> <br /> <br />
             <label htmlFor="">Lastname</label> <br />
-            <input type="text" name="" id="" /> <br /> <br />
+            <input type="text" name="lastname" id="" onChange={getAllData}/> <br /> <br />
             <label htmlFor="">Email</label> <br />
-            <input type="text" name="" id="" onChange={getEmail}/> <br /> <br />
-
+            <input type="text" name="email" id="" onChange={getAllData}/> <br /> <br />
+            <select name="gender" id="" onChange={getAllData}>
+                <option value="1">Male</option>
+                <option value="2">Female</option>
+                <option value="3">Others</option>
+                <option value="4">4</option>
+            </select>
             <button type='submit'>Submit</button>
         </form>
 
