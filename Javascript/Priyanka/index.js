ecab332ff5239f3f6238cb811af3526e96c8770d
Ravi Kumar <raviluhaniwal318@gmail.com>
2025-12-09 10:21:50 +0530

    js intro

---
+++ Javascript/Priyanka/index.js
@@ -0,0 +1,14 @@
+// Javascript :- Javascript is a programing language which is used to develop our web page.
+// it is a scripted , interpted and precomplied language.
+
+// output function
+
+console.log("Heloo Priyanka");
+console.warn("Warning")
+console.error("Error")
+
+// alert("Make a Alert")
+
+// user input
+prompt("Enter Your Name")
+
