14cb107df162984e1a7f0ddc953fc5d24f7ab3f4
Ravi Kumar <raviluhaniwal318@gmail.com>
2025-12-16 11:32:36 +0530

    express

---
+++ Node JS/Sandeep/README.md
@@ -0,0 +1,18 @@
+
+<!-- Mongoose :-
+
+MVC pattern : model view controller 
+
+Schema :- Structure of our Data/Product :
+Models 
+Controllers
+req
+res
+queries
+routes
+
+ -->
+
+
+
+
--- Node JS/Sandeep/index.js
+++ Node JS/Sandeep/index.js
@@ -3,12 +3,25 @@ const express = require("express");
 // Create a object
 const app = express();
 
+
+// Middlewares:- 
+app.use(express.json())
+
+
 // Get , Post , Put , Delete
+// To test Apis :- Thunder Client
 app.get("/api/getusers", (req, res) => {
   // res.send("Message send")
   res.json({
     message: "get api Craete success.",
     name: "Sandeep",
+  });
+});
+
+app.post("", (req, res) => {
+  console.log(req.body);
+  res.json({
+    message: "Post Request",
   });
 });
 
