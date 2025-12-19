d5e0d309c748a33fd1f6a7c335ea2b5a5021678e
Ravi Kumar <raviluhaniwal318@gmail.com>
2025-12-19 11:27:54 +0530

    loops

--- Javascript/Priyanka/index.js
+++ Javascript/Priyanka/index.js
@@ -4,11 +4,35 @@
 // output function
 
 console.log("Heloo Priyanka");
-console.warn("Warning")
-console.error("Error")
+console.warn("Warning");
+console.error("Error");
 
 // alert("Make a Alert")
 
 // user input
-prompt("Enter Your Name")
+prompt("Enter Your Name");
 
+// Loops :- it is a statement which is run as well as our condition is true.(cyclic process)
+
+// While loop
+// do-while loop
+// for loop
+
+// Initialise :-
+// condtion
+// increment / decrement
+let x = 0;
+
+// while (x <= 10) {
+//   console.log(x);
+//   x++;
+// }
+
+// do {
+//     console.log(x)
+//     x++
+// } while (x > 10);
+
+for (let y = 1; y > 30; y++) {
+  console.log(y);
+}
