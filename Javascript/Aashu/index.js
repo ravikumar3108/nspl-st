fa266ed05d57128f94283b0843d94924c639b543
Ravi Kumar <raviluhaniwal318@gmail.com>
2026-01-27 10:05:31 +0530

    js

---
+++ Javascript/Aashu/index.js
@@ -0,0 +1,20 @@
+// .js :- extension
+// javascript is a programing langauge which is used to develop our website.
+// it is a precompiled langauge , scripted and interpted langauge.
+// compiler :- which is convert our code into machine language(Binary number 0,1).
+// Server-Side
+// Output / Input methods .
+
+console.log("Heloo Aashu \n\n\n");
+
+prompt("Enter your name");
+
+console.warn("Warning");
+console.error("error");
+
+document.write("hello", "<br>");
+document.write("hello");
+document.write("hello");
+
+
+// Variables...
