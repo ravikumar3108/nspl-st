ccdaabae0caffb5a1c091e8a6bc4959368cab62b
Ravi Kumar <raviluhaniwal318@gmail.com>
2026-01-28 11:33:52 +0530

    cartdelete

--- React JS/sandeep/src/components/Nodejs/BackCart.jsx
+++ React JS/sandeep/src/components/Nodejs/BackCart.jsx
@@ -1,20 +1,54 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 function BackCart() {
+  const [CartItems, setCartItems] = useState([]);
+  console.log(CartItems);
+
   async function getCarts(e) {
     let data = await axios
       .get("http://localhost:8000/product/getCarts")
       .then((res) => {
-        console.log(res);
+        setCartItems(res.data.data);
       });
+  }
+
+  function deleteCartProduct(id) {
+    console.log(id);
+    const deletecart = axios.delete(`http://localhost:8000/product/deleteCartProduct/${id}`)
+    // window.reload()
   }
 
   useEffect(() => {
     getCarts();
   }, []);
 
-  return <div></div>;
+  return (
+    <>
+      <div>
+        {CartItems.map((item) => {
+          return (
+            <>
+              <div
+                style={{
+                  border: "1px solid",
+                  margin: "10px",
+                  width: "40%",
+                  padding: "10px",
+                }}
+              >
+                <p>{item._id}</p>
+                <p>{item.title}</p>
+                <button onClick={() => deleteCartProduct(item._id)}>
+                  Delete
+                </button>
+              </div>
+            </>
+          );
+        })}
+      </div>
+      ;
+    </>
+  );
 }
-
 export default BackCart;
