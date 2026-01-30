f493ce66a7ac7a367b3ed5d46a66ddc1fe02eb98
Ravi Kumar <raviluhaniwal318@gmail.com>
2026-01-30 10:10:39 +0530

    op

--- Javascript/Aashu/index.js
+++ Javascript/Aashu/index.js
@@ -1,124 +1,187 @@
-// // .js :- extension
-// // javascript is a programing langauge which is used to develop our website.
-// // it is a precompiled langauge , scripted and interpted langauge.
-// // compiler :- which is convert our code into machine language(Binary number 0,1).
-// // Server-Side
-// // Output / Input methods .
+// // // .js :- extension
+// // // javascript is a programing langauge which is used to develop our website.
+// // // it is a precompiled langauge , scripted and interpted langauge.
+// // // compiler :- which is convert our code into machine language(Binary number 0,1).
+// // // Server-Side
+// // // Output / Input methods .
 
-// console.log("Heloo Aashu \n\n\n");
+// // console.log("Heloo Aashu \n\n\n");
 
-// // prompt("Enter your name");
+// // // prompt("Enter your name");
 
-// console.warn("Warning");
-// console.error("error");
+// // console.warn("Warning");
+// // console.error("error");
 
-// document.write("hello", "<br>");
-// document.write("hello");
-// document.write("hello");
+// // document.write("hello", "<br>");
+// // document.write("hello");
+// // document.write("hello");
 
-// // Variables... :- it is used to store our data.
+// // // Variables... :- it is used to store our data.
 
-// // create a variable
+// // // create a variable
 
-// x = -10;
-// console.log("x is :", x);
+// // x = -10;
+// // console.log("x is :", x);
 
-// // keywords :- let , var , const
+// // // keywords :- let , var , const
 
-// // let
-// //  1. it is block scope.
-// {
-//   let a = 20;
-//   console.log("a", a);
-// }
-// // console.log(a)
+// // // let
+// // //  1. it is block scope.
+// // {
+// //   let a = 20;
+// //   console.log("a", a);
+// // }
+// // // console.log(a)
 
-// // 2. it cannot be re-declared.
+// // // 2. it cannot be re-declared.
 
-// let y = 10;
-// console.log(y + 10);
-// // re-assign.
-// y = 30;
-// console.log(y + 20);
+// // let y = 10;
+// // console.log(y + 10);
+// // // re-assign.
+// // y = 30;
+// // console.log(y + 20);
 
-// let y1 = 30;
+// // let y1 = 30;
 
-// // Var
-// // 1. it can be re-declared
-// // 2. it cannot be a block scope
-// {
-//   var bb = 3;
-// }
-// console.log(bb);
-// var b = 30;
-// var b = 40;
+// // // Var
+// // // 1. it can be re-declared
+// // // 2. it cannot be a block scope
+// // {
+// //   var bb = 3;
+// // }
+// // console.log(bb);
+// // var b = 30;
+// // var b = 40;
 
-// // const :- constant
-// // 1. it cannot be re-declared.
-// // 2. it cannot be re-assign.
-// // 3. it can be a block scope.
-// const k = 20;
-// // k = 40;
+// // // const :- constant
+// // // 1. it cannot be re-declared.
+// // // 2. it cannot be re-assign.
+// // // 3. it can be a block scope.
+// // const k = 20;
+// // // k = 40;
 
-// // Variables names / identifiers
-// // 1. Alphabets (a-z)(A-Z)
-// // Case-sensitive
-// let j = 20;
-// let J = 40;
-// console.log(j);
-// console.log(J);
-// // 2. Names
-// let aashu = 40;
-// // 3. underscore
-// let _aashu20 = 30;
-// // 4. alphabtes + number
-// let a10 = 6;
-// let aashhu30 = 3;
-// // 5. camelCase
-// let javaScriptCss;
-// let aashuYadav;
-// // 6. Pascal Case
-// let AshuYadavv;
-// // 7. Snake case
-// let aashu_yadav_kumar_;
-// let AASHU;
+// // // Variables names / identifiers
+// // // 1. Alphabets (a-z)(A-Z)
+// // // Case-sensitive
+// // let j = 20;
+// // let J = 40;
+// // console.log(j);
+// // console.log(J);
+// // // 2. Names
+// // let aashu = 40;
+// // // 3. underscore
+// // let _aashu20 = 30;
+// // // 4. alphabtes + number
+// // let a10 = 6;
+// // let aashhu30 = 3;
+// // // 5. camelCase
+// // let javaScriptCss;
+// // let aashuYadav;
+// // // 6. Pascal Case
+// // let AshuYadavv;
+// // // 7. Snake case
+// // let aashu_yadav_kumar_;
+// // let AASHU;
 
-// // Data Types :-
+// // // Data Types :-
 
-// 1. primitive data types :- which is store single data
-// integer
-let x = 20;
-// string :- string are used to store character values.
-// string used into single and double quotes
-let y = "aashu";
-let y1 = "aashu";
+// // 1. primitive data types :- which is store single data
+// // integer
+// let x = 20;
+// // string :- string are used to store character values.
+// // string used into single and double quotes
+// let y = "aashu";
+// let y1 = "aashu";
 
-let num = "10a";
+// let num = "10a";
 
-// boolean :-
-let b = true;
-let b1 = false;
+// // boolean :-
+// let b = true;
+// let b1 = false;
 
-// null :-
-let x1 = null;
+// // null :-
+// let x1 = null;
 
-// undefined
-let ab;
-console.log(ab);
+// // undefined
+// let ab;
+// console.log(ab);
 
-// 2. non-primitive data types :- which is store multiple data
-// array :- array is used to store multiple values.
+// // 2. non-primitive data types :- which is store multiple data
+// // array :- array is used to store multiple values.
 
-let arr = [1, 2, 4, 4, 5, 67, 8, 9, 9, "data", true, null, {}];
-console.log(arr);
+// let arr = [1, 2, 4, 4, 5, 67, 8, 9, 9, "data", true, null, {}];
+// console.log(arr);
 
-// object ;- object used to store multiple used but in key and values.
+// // object ;- object used to store multiple used but in key and values.
 
-let obj = {
-  id: 1,
-  name: "aashu",
-  course: "web designing",
-  rollno: 20,
-};
+// let obj = {
+//   id: 1,
+//   name: "aashu",
+//   course: "web designing",
+//   rollno: 20,
+// };
 
-console.log(obj.name);
+// console.log(obj.name);
+
+// Operators
+// 1. Arithmetic OP
+// +
+// -
+// *
+// /
+// ** = expontation (power)
+// % = modulus (remainder)
+// ++ increment
+// -- decrement
+
+let num = 10;
+let num1 = 20;
+let num2 = "10";
+
+let add = num + num1;
+
+console.log(num + num1);
+console.log(num + num2);
+console.log(add + 20);
+
+let x = 21;
+let y = 3;
+// 2 ki power 3
+console.log(x ** y);
+
+console.log(x % y);
+
+let a = 10;
+// prefix and postfix
+let xy = ++a;
+console.log(xy);
+console.log(a);
+
+// a = 11
+let yy = a++;
+console.log(yy);
+console.log(a);
+
+// 2. Assignment OP
+// =
+// +=
+// -=
+// *=
+// /=
+// **=
+// %=
+
+let num3 = 10;
+// let num4 = num3 + 10;
+num3 += 20;
+num3 *= 20;
+
+num3 **= 2;
+
+// console.log(num4);
+console.log(num3);
+
+// 3. Comparision OP
+
+
+// 4. Logical OP
