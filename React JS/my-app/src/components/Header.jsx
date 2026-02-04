ce61fbb2bdd7b3d64d94d2031e1d7206e63e6c3d
Ravi Kumar <raviluhaniwal318@gmail.com>
2026-02-04 10:41:17 +0530

    function comp

---
+++ React JS/my-app/src/components/Header.jsx
@@ -0,0 +1,16 @@
+// Create a function components
+
+import React from "react";
+
+function Header() {
+  return (
+    // JSX fragments :- which is allow to create Html elements within the javascript code.
+    <>
+      <div className="header">
+        <h1>Learning React JS</h1>
+      </div>
+    </>
+  );
+}
+
+export default Header;
