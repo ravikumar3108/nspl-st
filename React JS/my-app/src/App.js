7767b3024edf38906a45a36d1221801ce7d023d8
Ravi Kumar <raviluhaniwal318@gmail.com>
2026-02-05 10:39:12 +0530

    img

--- React JS/my-app/src/App.js
+++ React JS/my-app/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   return (
@@ -8,11 +9,8 @@ function App() {
       {/* Call the funtion components */}
       {/* Reusable */}
       <Header />
-      <Header />
-      <Header />
-      <h1>Heloo</h1>
-      <Header />
-      <Header />
+      <Home/>
+      
     </>
   );
 }
---
+++ React JS/my-app/src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import img from "./images/blog5.jpeg";
+
+function Home() {
+  let x = "Neeraj";
+
+  return (
+    <>
+      <h1 className="home" style={{ color: "green", paddingTop: "10px" }}>
+        Home Component
+      </h1>
+      <h2>Heloo {x}</h2>
+      <img src={img} alt="" width={"100%"} height={""} />
+    </>
+  );
+}
+
+export default Home;
