4b907df5b0444f6a18b5da1b7d39ff3fef06f1d9
Ravi Kumar <raviluhaniwal318@gmail.com>
2026-03-19 16:23:55 +0530

    database connect

--- Node JS/Rajni/index.js
+++ Node JS/Rajni/index.js
@@ -2,12 +2,25 @@
 // Common js : node js
 
 const express = require("express");
+const mongoose = require("mongoose");
 // App :- object
 const app = express();
+
+// Mongodb Connectivity :-
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb+srv://ravikumar:Ravi123@cluster0.cy4n69o.mongodb.net/?appName=Cluster0");
+  console.log("Database connect")
+}
 
 // Api's :- GET , POST , PUT , DELETE
 // req :- request
 // res :- response
+
+// Middlewares :-
+app.use(express.json());
 
 app.get("", (req, res) => {
   // res.send("Message")
@@ -15,6 +28,11 @@ app.get("", (req, res) => {
 });
 
 // Callback functions :-
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.json({ message: "true" });
+});
 
 app.listen(8000, () => {
   console.log("Server Create");
