879871f4b770a29b8df5ac3672798d16532ff053
Ravi Kumar <raviluhaniwal318@gmail.com>
2026-03-24 09:44:39 +0530

    c lang

---
+++ C & C++/Aashu/demo.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// std :- standard library 
+// io :- input and output function
+// h : header file
+
+// most important :- semicolons (exit the line)
+
+int main(){
+	
+	// output function 
+	
+	printf("Hello Aashu");
+	
+	
+	
+	
+	
+	return 0; // exit the main function 
+}
