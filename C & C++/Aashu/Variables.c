c510e0cd26c1c83f8477e951fd74cf67438e55a3
Ravi Kumar <raviluhaniwal318@gmail.com>
2026-03-26 09:36:33 +0530

    data

--- C & C++/Aashu/Variables.c
+++ C & C++/Aashu/Variables.c
@@ -6,55 +6,104 @@ int main(){
 	
 //	int :- interger value -1 and 1
 // float :- decimal values -0.1 and 0.23
-// char :- store only single character. 
+//// char :- store only single character. 
+//
+//
+//int x = 10;
+//float y = 10.2;
+//char z = 'A';
+//
+//// format specifiers :- specilise a data.
+//// integer :- %d or %i
+//// float :- %f
+//// char :- %c 
+//
+//printf("%i \n", x);
+//printf("%f \n",y);
+//printf("%c",z);
+//
+//
+//// Assign variables 
+//
+//int num = 29;
+//int num2 = 34;
+//
+//num2 = num;
+//printf("%d \n",num2);
+//
+//
+//// declare multiple variables :- 
+//int a , b , c ;
+//
+//a = b = c = 20;
+//
+//printf("%d",a);
+//
+//// identifiers :- names of our varibales.
+//// names or alphabets A-Z or a-z
+//// int + alpha 
+//// camel case
+//// pascal case 
+//// varibale name not start with a  number.
+//// underscore 
+//// dollar
+//
+//int $k = 20;
+//
+//printf("%d \n",$k); 
+//
+//printf("%d \n", num + num2);
+//
+//
+//// Data types :- 
+//// interger :- size in bytes(2 or 4 byte)
+//// float :- size in bytes (4 bytes)
+//// double :- size in bytes(8 bytes)
+//// format specifier :- %lf 
+////char : size in bytes (1 bytes)
+//
+//int s = 10;
+//
+//// to find a size of our data type
+//printf("%d \n",sizeof(s));
 
 
-int x = 10;
-float y = 10.2;
-char z = 'A';
+// operators :- 
+// Assignments op 
+// + ,- , * , / , % , ++ , --
 
-// format specifiers :- specilise a data.
-// integer :- %d or %i
-// float :- %f
-// char :- %c 
+float  x = 11;
+float y = 2;
 
-printf("%i \n", x);
-printf("%f \n",y);
-printf("%c",z);
+printf("%f \n", x / y);
 
 
-// Assign variables 
+// Assignment OP
+//=  
+// +=
+// -=
+// *=
+// /=
+// %= 
 
-int num = 29;
-int num2 = 34;
+int z = 10;
+z += 20;
 
-num2 = num;
-printf("%d \n",num2);
-
-
-// declare multiple variables :- 
-int a , b , c ;
-
-a = b = c = 20;
-
-printf("%d",a);
-
-// identifiers :- names of our varibales.
-// names or alphabets A-Z or a-z
-// int + alpha 
-// camel case
-// pascal case 
-// varibale name not start with a  number.
-// underscore 
-// dollar
-
-int $k = 20;
-
-printf("%d \n",$k); 
-
-printf("%d \n", num + num2);
+printf("%d",z);
 
 
+// comparision :- (true and false) (0,1)
+// ==
+// != 
+// <= 
+// >= 
+// <
+// >
+
+// logical op
+// and :- && 
+// or :- ||  
+// not :- !
 
 
 
