3c5e8cd00c2d1a5e60bb4280ec597f2d901c11aa
Ravi Kumar <raviluhaniwal318@gmail.com>
2026-03-30 16:15:41 +0530

    back

---
+++ Mern Project/student node/Readme.md
@@ -0,0 +1,7 @@
+// Class :- 2 
+// Module install mongoose
+// Step 1 :- Connectivity of our database.
+Step : 2  Create a schema 
+Step : 3 Create a model 
+Use try and catch into the Apis
+Step : 4 Save the Data by the Model into our Post Api(Signup)
---
+++ Mern Project/student node/index.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(
+    "mongodb+srv://ravikumar:Ravi123@cluster0.cy4n69o.mongodb.net/?appName=Cluster0",
+  );
+  console.log("DataBase Connect");
+}
+
+// Middlewares :-
+app.use(express.json());
+app.use(cors());
+// Cors :- Cross-Origin Resource Sharing (CORS) is an HTTP-header based browser security mechanism that
+// allows a server to permit resources (e.g., fonts, APIs)
+// to be loaded by web applications on different domains, overcoming the same-origin policy.
+
+// Mongodb :-
+// Create a User Schema
+
+const userschema = new mongoose.Schema({
+  username: {
+    type: String,
+    // unique: true,
+    // trim: true,
+  },
+  email: {
+    type: String,
+  },
+  name: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+});
+
+// Create a Model :-
+const User = mongoose.model("user", userschema);
+
+// Api:- Create a user (Signup)
+app.post("/signup", async (req, res) => {
+  // Step:1 :- Check the data. (req.body)
+  // console.log(req.body);
+
+  try {
+    // this data belongs to frontend
+    const { email, username, name, password } = req.body;
+
+    const createuser = new User({
+      // left side :- schema name
+      // right side :- body name
+      email: email,
+      username: username,
+      name: name,
+      password: password,
+    });
+
+    // save the data
+    const saveuser = await createuser.save();
+
+    if (saveuser) {
+      res
+        .status(200)
+        .json({ message: "Success", status: true, user: saveuser });
+    } else {
+      res.status(500).json({ message: "error", status: false });
+    }
+  } catch (error) {
+    res.json({ error: error, message: "Error in Signup" });
+  }
+
+  //   res.json({ message: "Signup", status: true });
+});
+
+// to check a server
+app.get("", (req, res) => {
+  res.json({ message: "Success", status: true });
+});
+
+// Create a Server :
+app.listen(8000, (req, res) => {
+  console.log("Server Start");
+});
