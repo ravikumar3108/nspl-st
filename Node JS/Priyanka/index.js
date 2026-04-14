b221e5c23cbff5de379949defdce172ac7649cb1
Ravi Kumar <raviluhaniwal318@gmail.com>
2026-04-14 11:01:41 +0530

    nodejs

---
+++ Node JS/Priyanka/index.js
@@ -0,0 +1,20 @@
+console.log("Heloo Priyanka")
+console.log("Heloo Priyanka")
+console.log("Heloo Priyanka")
+console.log("Heloo Priyanka")
+console.log("Heloo Priyanka")
+console.log("Heloo Priyanka")
+console.log("Heloo Priyanka")
+
+
+let x = 30
+console.log(x)
+console.log(x)
+console.log(x)
+console.log(x)
+console.log(x)
+
+
+
+
+
