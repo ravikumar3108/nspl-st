6201539eba7aec2e1cd7740e9b0028d15b80b1de
Ravi Kumar <raviluhaniwal318@gmail.com>
2026-04-15 10:51:37 +0530

    node

--- Node JS/Priyanka/index.js
+++ Node JS/Priyanka/index.js
@@ -1,20 +1,22 @@
-console.log("Heloo Priyanka")
-console.log("Heloo Priyanka")
-console.log("Heloo Priyanka")
-console.log("Heloo Priyanka")
-console.log("Heloo Priyanka")
-console.log("Heloo Priyanka")
-console.log("Heloo Priyanka")
+console.log("Heloo node js");
+console.log("Heloo node js");
 
+// Create a server
+// Common js
+// left side :  variables
+// right side :- package name
+const express = require("express");
+// Create a object.
+const app = express();
 
-let x = 30
-console.log(x)
-console.log(x)
-console.log(x)
-console.log(x)
-console.log(x)
+// Api's :- GET , POST , PUT , DELETE
 
+app.get("", (req, res) => {
+  res.json({status:"true",data:[{id:"1"}]});
+});
 
-
-
-
+// Create a server
+// Callback function....
+app.listen(8000, () => {
+  console.log("Server Create Successfull...");
+});
