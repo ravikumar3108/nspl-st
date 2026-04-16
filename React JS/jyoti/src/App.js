7048dbfd16cc0bd388571463a06cbb7bd4a81e9a
Ravi Kumar <raviluhaniwal318@gmail.com>
2026-04-16 15:25:38 +0530

    react

---
+++ React JS/jyoti/src/App.js
@@ -0,0 +1,20 @@
+// Function Component
+import React from "react";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    // Jsx fragments
+    <>
+      <Header />
+      <Header />
+      <Header />
+      <Header />
+      <Header />
+      <Header />
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ React JS/jyoti/src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Header() {
+    return (
+        <>
+            <h1>
+
+            </h1>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint corrupti, nulla placeat quaerat fuga minus repudiandae saepe nobis exercitationem? Aperiam neque provident sequi, sit numquam dolores delectus at consequatur quam!
+            </p>
+
+            <div>
+                <p></p>
+            </div>
+
+        </>
+    )
+}
+
+export default Header;
