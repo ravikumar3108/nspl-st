8734f4d269f0a8328c6de2db4346e7be837ab9fd
Ravi Kumar <raviluhaniwal318@gmail.com>
2026-04-23 10:50:24 +0530

    :post

--- Node JS/Priyanka/index.js
+++ Node JS/Priyanka/index.js
@@ -15,13 +15,19 @@ main().catch((err) => console.log(err));
 
 async function main() {
   await mongoose.connect(
-    "mongodb+srv://ravikumar:Ravi123@cluster0.cy4n69o.mongodb.net/?appName=Cluster0",
+    "mongodb+srv://ravikumar:Ravi123@cluster0.cy4n69o.mongodb.net/priyanka?appName=Cluster0",
   );
   console.log("Databse Connected");
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
+const studentSchema = new mongoose.Schema({
+  firstname: String,
+  lastname: String,
+  email: String,
+});
 
+const Student = mongoose.model("student", studentSchema);
 
 // Middlewares :-
 app.use(express.json());
@@ -32,9 +38,19 @@ app.get("", (req, res) => {
   res.json({ status: "true", data: [{ id: "1" }] });
 });
 
-app.post("", (req, res) => {
+app.post("/createStudent", async (req, res) => {
   console.log(req.body);
-  res.json({ message: "Success", data: req.body });
+
+  const { firstname, lastname, email, fathername, mobilno, address } = req.body;
+
+  const createStudent = new Student({
+    firstname: firstname,
+    lastname: lastname,
+    email: email,
+  });
+
+  const saveUser = await createStudent.save();
+  res.json({ message: "Success", data: saveUser });
 });
 
 // Create a server
