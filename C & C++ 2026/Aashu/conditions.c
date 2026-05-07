470186224d92bf3ed558fa929eea2196b22b4ed1
Ravi Kumar <raviluhaniwal318@gmail.com>
2026-05-07 09:47:21 +0530

    loops

--- C & C++ 2026/Aashu/conditions.c
+++ C & C++ 2026/Aashu/conditions.c
@@ -3,7 +3,7 @@
 int main(){
 	
 	int x = 0;
-	if( x == 0){
+	if(x == 0){
 		printf("True");
 	}
 	else if(x > 0){
@@ -15,6 +15,7 @@ int main(){
 	
 	
 //	short hand condition :- 
+//Ternary operators
 
 	(x == 0) ? printf("first \n") : printf("second \n");
 	
@@ -30,6 +31,48 @@ int main(){
 	else{
 		printf("Else");
 	}
+	
+	
+	// loops :-  it is a statement which is run when our condition is as well as true..  
+	
+	// while loop 
+	// do-whilw loop :- 
+	// for loop :- 
+	
+	// initialse a variable; 
+	int a = 0;
+	
+	while(a <= 10){
+		printf("%d\n",a);
+		a++;
+	}
+	
+	// break and continue 
+	
+//	int b;
+//	for(b = 0;b < 20;b++){
+//		if(b == 10){
+//			break;
+//		}
+//		printf("%d\n",b);
+//	}
+	
+	
+	int b;
+	for(b = 0;b < 20;b++){
+		if(b == 10){
+			continue;
+		}
+		printf("%d\n",b);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
