3232450387fa64cf7566a56ae34f01412a56dc9b
Ravi Kumar <raviluhaniwal318@gmail.com>
2026-05-09 09:51:47 +0530

    fun

---
+++ C & C++ 2026/Aashu/memeoryand pointer.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <string.h>
+
+// declare a function 
+void myfun(){
+	printf("Function is running.... \n");
+}
+
+// Parameters :- 
+void myfun2(int age,char name[]){
+	printf("%d \n",age);
+	printf("%s \n",name);
+	
+}
+
+// int function :- which is gives a return value....
+
+int Aashu(){
+	return 10 * 10;
+}
+
+// default values....
+
+int main(){
+		// Function :- it is a block of code which is run when our function is call...
+	// void function :- that means the function is not givin any return value...
+	// Functions are reusabale
+	
+	myfun();
+	myfun();
+	myfun();
+	myfun();
+	myfun2(23,"AAshu");
+	int c = Aashu();
+	printf("%d \n",c);
+	
+	
+//	Memory Address :- when a varible is create then it store into our memory.... 
+	// To find the mmory address we can use use a refrence & 
+	
+	int x = 90;
+	int y = 10;
+	printf("%p \n",&x);
+	printf("%p \n",&y);
+
+
+// Pointer :- which is store our memory address..
+
+int a = 10;
+
+printf("%d \n",a);
+
+printf("%p \n",&a);
+
+
+int* pt = &a;
+printf("%p \n",pt);
+	
+	return 0;
+}
