b67ab6fdbdd3c355a6f1076c97d28bb4a767b386
Ravi Kumar <raviluhaniwal318@gmail.com>
2026-05-30 14:42:19 +0530

    intro

---
+++ Node JS/Jyoti/index.js
@@ -0,0 +1,16 @@
+// console.log("Run File...")
+// console.log("Run File...")
+// console.log("Run File...")
+// console.log("Run File...")
+// console.log("Run File...")
+
+// Create a Server.....
+const express = require("express");
+
+const app = express();
+
+// PORT Number
+
+app.listen(8000, () => {
+  console.log("Server Create.");
+});
