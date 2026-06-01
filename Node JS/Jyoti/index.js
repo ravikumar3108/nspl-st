1b86c95dde93957126973ff194c47d4499832cdc
Ravi Kumar <raviluhaniwal318@gmail.com>
2026-06-01 10:22:38 +0530

    api

--- Node JS/Jyoti/index.js
+++ Node JS/Jyoti/index.js
@@ -9,8 +9,22 @@ const express = require("express");
 
 const app = express();
 
-// PORT Number
+// Middlewares :-
+app.use(express.json());
 
+// Api's :- Get , post , put , delete
+
+app.get("", (req, res) => {
+  res.json({ status: true, message: [], data: [] });
+});
+
+// Post Api
+app.post("", (req, res) => {
+  console.log(req.body);
+  res.json({ message: "Post Api" });
+});
+
+// PORT Number
 app.listen(8000, () => {
   console.log("Server Create.");
 });
