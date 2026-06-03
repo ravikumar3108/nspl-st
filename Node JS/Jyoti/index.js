a0b270ba0f21dc5e3de8c5f543148ae9af1f3799
Ravi Kumar <raviluhaniwal318@gmail.com>
2026-06-03 14:54:20 +0530

    database connect

--- Node JS/Jyoti/index.js
+++ Node JS/Jyoti/index.js
@@ -6,22 +6,43 @@
 
 // Create a Server.....
 const express = require("express");
+const mongoose = require("mongoose");
+
+main().catch((err) => console.log(err));
 
 const app = express();
+async function main() {
+  await mongoose.connect(
+    "mongodb+srv://ravikumar:Ravi123@cluster0.cy4n69o.mongodb.net/Jyoti?appName=Cluster0",
+  );
+  console.log("Database Connected.");
+  // localhost  : mongodb://127.0.0.1:27017
+}
 
 // Middlewares :-
 app.use(express.json());
 
 // Api's :- Get , post , put , delete
 
-app.get("", (req, res) => {
+app.get("/api/get", (req, res) => {
   res.json({ status: true, message: [], data: [] });
 });
 
+// Schema :-
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  age: Number,
+});
+
+const userModel = mongoose.model("user", userSchema);
+
+
 // Post Api
-app.post("", (req, res) => {
+app.post("/api/post", (req, res) => {
   console.log(req.body);
-  res.json({ message: "Post Api" });
+  res.json({ message: "Post Api", data: req.body });
 });
 
 // PORT Number
