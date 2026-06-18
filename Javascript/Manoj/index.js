3228a1a62b143a6aeaa970a8593f97052abc8781
Ravi Kumar <raviluhaniwal318@gmail.com>
2026-06-18 17:40:34 +0530

    js

---
+++ Javascript/Manoj/index.js
@@ -0,0 +1,20 @@
+// JavaScript is the programming language of the web Application.
+// It can calculate, manipulate and validate data. It can update and change both HTML and CSS.
+
+// JavaScript (JS) is a high-level, interpreted programming language used to create dynamic, interactive content on the web
+// scripted
+// precompiled :- compiler(convert our code into machine language(binary numbers(0,1)))
+// interpreted
+
+// Interpreted Language: Code executes line-by-line in real time without compilation.
+// Dynamically Typed: Variable types automatically determine themselves at runtime.
+// Single-Threaded: Executes exactly one task at a time.
+// Event-Driven: Code responds directly to clicks, keystrokes, and hover actions.
+// Cross-Platform: Runs inside every modern browser out of the box.
+
+// Javascript :-
+// 1. Internal js :- head / body 
+// 2. External Js
+
+// Output Function :---
+console.log("Heloo Manoj Singh");
