c4279831cd7b69112a945394ea0ae8c0d74805da
Ravi Kumar <raviluhaniwal318@gmail.com>
2026-06-19 17:35:27 +0530

    variables

--- Javascript/Manoj/index.js
+++ Javascript/Manoj/index.js
@@ -13,8 +13,54 @@
 // Cross-Platform: Runs inside every modern browser out of the box.
 
 // Javascript :-
-// 1. Internal js :- head / body 
+// 1. Internal js :- head / body
 // 2. External Js
 
 // Output Function :---
 console.log("Heloo Manoj Singh");
+
+// Variables :- it is used to store our data...
+
+// keyword : let , const
+
+// Create a varible.. (declare)
+
+let x = 10;
+
+// x :- variable name
+// 10 :- data/value
+console.log("x is ", x);
+
+// re-declared is not allowed in let keyword..
+// let x = 20;
+
+// re-assign :- true
+x = 20;
+console.log("x is", x);
+
+const y = 20;
+
+// re-declared is not allowed in let keyword..
+// const y = 20;
+
+// re-assign :- false
+// y = 30
+
+// identifiers :- Variable names
+// 1. a-z or A-Z (alphabets)
+// 2. alphbets + number
+const y10 = 30;
+// 3. camel case
+let manojSingh = 20;
+// 4. Pascal case:-
+let DryFruits = 30;
+// 5.snake case
+let dry_fruits = 29;
+// 6. Name
+
+// 7. Case-sensitive
+let manoj = 20;
+console.log(manoj);
+let MANOJ = 30;
+console.log(MANOJ);
+//
